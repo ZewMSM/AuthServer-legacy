c732784977355ec1f8dbdd802a5cf5984ee2fdd9
Zewsic <66947765+Zewsic@users.noreply.github.com>
2024-11-17 20:43:53 +0300

    fix logging in servers !deploy

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import asyncio
 import logging
+from os import environ
 
 from auth_server import start_auth_server
 from database import init_database
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG if environ.get('type', 'release') == 'debug' else logging.INFO,
     format='%(levelname)s/%(name)s:\t%(message)s'
 )
 
